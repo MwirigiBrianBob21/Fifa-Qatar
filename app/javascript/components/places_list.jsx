e81b09ce7c2344ae5233bfcb12affe56da6f6b63
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-12-06 16:05:23 +0300

    Add stadium list

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
 
-class PlaceList extends React.Component {
-    render() {
-        return (
-            <div> Places list rendered in react</div>
-        )
+function PlacesList() {
+    const [loading, setLoading] = useState(true)
+    const[loadedStadiums, setLoadedStadiums] = useState([])
+    
+    const loadingSection = (<div>loading</div>) 
+    const dataSection = loadedStadiums.map((stadium, index)=>
+
+    <div key={index}>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>City</th>
+                <th>Recent Stadium Capacity</th>
+                <th>Recent Stadium Temperature</th>
+                <th>Number of measurements</th>
+            </tr>
+            <tr>
+                <td>{stadium.name}</td>
+                <td>{stadium.city}</td>
+                <td>{stadium.stadium_capacity}</td>
+                <td>{stadium.stadium_temperatures}</td>
+                <td>{stadium.number_of_measurements}</td>
+            </tr>
+        </table>
+    </div>
+    )
+
+    if (loading){
+        return   loadingSection
+        
+    } else{
+        return dataSection
     }
+
+  
 }
 
-const placeList = ReactDOM.createRoot(document.getElementById("places-list-container"));
-placeList.render(<PlaceList />)
+// export default PlacesList
+
+// class PlaceList extends React.Component {
+//     render() {
+//         return (
+//             <div> Places list rendered in react</div>
+//         )
+//     }
+// }
+
+const placesList = ReactDOM.createRoot(document.getElementById("places-list-container"));
+placesList.render(<PlacesList />)
