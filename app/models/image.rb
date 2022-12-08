c320e08840fb633943565469a7b782000d0929ae
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-12-08 21:43:00 +0300

    Add validation to create images for Stadium

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,4 @@
 class Image < ApplicationRecord
-      # The URL of the image
-  # attr_accessor :url
-
-  # The description of the image
-  # attr_accessor :description
 
   belongs_to :stadium
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,4 +19,24 @@ stadia.each do |stadium|
   Stadium.create!(stadium)
 end
 
+images = [
+  { url: "image1.jpg", description: "Image 1", stadium_id: 1 },
+  { url: "image2.jpg", description: "Image 2", stadium_id: 1 },
+  { url: "image3.jpg", description: "Image 3", stadium_id: 2 },
+  { url: "image4.jpg", description: "Image 4", stadium_id: 3 },
+  { url: "image5.jpg", description: "Image 5", stadium_id: 4 }
+]
+
+images.each do |image|
+  # Make sure that the stadium_id value corresponds to an existing Stadium record
+  stadium = Stadium.find_by(id: image[:stadium_id])
+  if stadium
+    Image.create!(image)
+  end
+end
+
+# temperatures.each do |temp|
+# Temperature.create!(image)
+# end
+
 puts "Done seeding"
